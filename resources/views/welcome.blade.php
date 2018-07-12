f950f66d32fe8bfd5423d6676d443bd21748f57a
SajeewaJayarathne <sajeewagj@gmail.com>
2018-07-12 21:51:27 +0530

    adding search bar

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,5 +5,26 @@
 @endsection
 
 @section('content')
-    Content here!!!
+    <div id="search-bar" class="row" style="height: 20%; padding-top: 5%; padding-bottom: 5%">
+        <div class="col-2"></div>
+        <div class="col-8">
+            <form class="card p-2">
+                <div class="input-group">
+                    <input type="text" class="form-control" placeholder="Enter Keyword(s)">
+                    <div class="input-group-append">
+                        <button type="submit" class="btn btn-secondary">
+                            <i class="fa fa-search"></i>
+                        </button>
+                    </div>
+                </div>
+            </form>
+        </div>
+        <div class="col-2"></div>
+    </div> 
+    
+    <div class="results-container">
+        <div class="row">
+            <h2>Search</h2>
+        </div>
+    </div>
 @endsection
--- storage/framework/views/63a8e89d951cd6debc83b218da444b18cc80148c.php
+++ storage/framework/views/63a8e89d951cd6debc83b218da444b18cc80148c.php
@@ -3,6 +3,27 @@
 <?php $__env->stopSection(); ?>
 
 <?php $__env->startSection('content'); ?>
-    Content here!!!
+    <div id="search-bar" class="row" style="height: 20%; padding-top: 5%; padding-bottom: 5%">
+        <div class="col-2"></div>
+        <div class="col-8">
+            <form class="card p-2">
+                <div class="input-group">
+                    <input type="text" class="form-control" placeholder="Enter Keyword(s)">
+                    <div class="input-group-append">
+                        <button type="submit" class="btn btn-secondary">
+                            <i class="fa fa-search"></i>Search
+                        </button>
+                    </div>
+                </div>
+            </form>
+        </div>
+        <div class="col-2"></div>
+    </div> 
+    
+    <div class="results-container">
+        <div class="row">
+            <h2>Search</h2>
+        </div>
+    </div>
 <?php $__env->stopSection(); ?>
 <?php echo $__env->make('layouts.master', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
