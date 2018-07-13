aaa7263259bc03e783a1ec6b074365ba5ac3d37a
SajeewaJayarathne <sajeewagj@gmail.com>
2018-07-13 13:19:36 +0530

    changes in search bar

---
+++ resources/views/includes/search-bar.blade.php
@@ -0,0 +1,24 @@
+<div class="col-2"></div>
+<div class="col-8">
+    {{-- <form class="card p-2">
+        <div class="input-group">
+            <input type="text" class="form-control" placeholder="Enter Keyword(s)">
+            <div class="input-group-append">
+                <button type="submit" class="btn btn-secondary">Search
+                    <i class="fa fa-search"></i>
+                </button>
+            </div>
+        </div>
+    </form> --}}
+    <div id="custom-search-input">
+        <div class="input-group col-md-12">
+            <input type="text" class="search-query form-control" placeholder="Enter Keyword(s)" />
+            <span class="input-group-btn">
+                <button class="btn btn-danger" type="button">
+                    <span class="glyphicon glyphicon-search"></span>
+                </button>
+            </span>
+        </div>
+    </div>
+</div>
+<div class="col-2"></div>
