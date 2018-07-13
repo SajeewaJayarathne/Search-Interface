9a73f4169072677a0ccafa432000548d4c2e6825
SajeewaJayarathne <sajeewagj@gmail.com>
2018-07-13 15:57:49 +0530

    adding company logo and favicon

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,11 +1,13 @@
 <header>
     <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom box-shadow">
-        <h5 class="my-0 mr-md-auto font-weight-normal">Company logo</h5>
-        <nav class="my-2 my-md-0 mr-md-3">
-          <a class="p-2 text-dark" href="#">Faculties and Schools</a>
-          <a class="p-2 text-dark" href="#">Courses</a>
-          <a class="p-2 text-dark" href="#">Research</a>
-          <a class="p-2 text-dark" href="#">A-Z</a>
+        <div class="my-0 mr-md-auto">
+            <img src="{{URL::asset('/images/company_logo.png')}}" alt="" style="max-height: 75px">
+        </div>
+        <nav class="my-2 my-md-0 mr-md-4" style="font-size: 2.5ex">
+            <a class="p-2 text-dark" href="#">Faculties and Schools</a>
+            <a class="p-2 text-dark" href="#">Courses</a>
+            <a class="p-2 text-dark" href="#">Research</a>
+            <a class="p-2 text-dark" href="#">A-Z</a>
         </nav>
-      </div>
+    </div>
 </header>
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,7 +4,8 @@
         <title>@yield('title')</title>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-        <link rel="stylesheet" type="text/css" href="../../../public/css/search-bar.css">
+        {{-- <link rel="stylesheet" type="text/css" href="../../../public/css/search-bar.css"> --}}
+        <link rel="shortcut icon" href="{{ asset('favicon.png') }}">
     </head>
 
     <body>
