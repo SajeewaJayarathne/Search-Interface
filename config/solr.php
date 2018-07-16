4f98d186dd77ddc433ba7d51ec7828aea3fd4d8c
kusal-rabbie <kusal.14@cse.mrt.ac.lk>
2018-07-16 21:48:10 +0530

    added groupsize paramter

--- config/solr.php
+++ config/solr.php
@@ -1,11 +1,11 @@
 <?php
 return [
-                         'df' => 'name',
-                         'rows' => 10,
-                         'fl' => array('name','gi_website','services'),
-                         'omitHeader' => 'true',
-                         'wt' => 'json',
-                         'group' => 'true',
-                         'groupField' => 'category',
-                         'groupLimit' => '10',
+    'df' => 'name',
+    'rows' => 10,
+    'fl' => array('name','gi_website','services'),
+    'omitHeader' => 'true',
+    'wt' => 'json',
+    'group' => 'true',
+    'groupField' => 'category',
+    'groupLimit' => '10',
 ];
