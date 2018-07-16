b165b31ee5675800bd99507fd5adc092888b8166
kusal-rabbie <kusal.14@cse.mrt.ac.lk>
2018-07-16 16:05:24 +0530

    results view added

--- resources/views/includes/results.blade.php
+++ resources/views/includes/results.blade.php
@@ -2,6 +2,8 @@
     <span style="color: gray">
        @foreach ($results as $result )
            <li> {{ $result->name }} </li>
+           {{-- <li> {{ $result->gi_website}}</li> --}}
+           {{-- <li> {{ $result->services_-_services}}</li> --}}
        @endforeach
     </span>
 </body>
